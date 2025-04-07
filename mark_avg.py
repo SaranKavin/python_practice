185f35ca42df69bb031f53fd80c3e7cdc5fb4576
SARAN P <138579247+SaranKavin@users.noreply.github.com>
2025-04-07 23:14:56 +0530

    Create mark_avg.py

---
+++ mark_avg.py
@@ -0,0 +1,8 @@
+mark1=int(input("enter your mark:"))
+mark2=int(input("enter your mark:"))
+mark3=int(input("enter your mark:"))
+mark4=int(input("enter your mark:"))
+mark5=int(input("enter your mark:"))
+a=mark1+mark2+mark3+mark4+mark5
+print("total mark",a,"/500")
+print(a/5,"%")
